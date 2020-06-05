b6fe43b2782e49b2f7b1d2411d1a6e55fcf9bdef
Dennis James Matildo <dj.matildo@gmail.com>
2020-06-05 21:51:30 +0800

    Apply BCRYPT and SALT and Compare Account Credential

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 
@@ -9,10 +10,34 @@ app.get('/users', (req, res) => {
     res.json(users);
 });
 
-app.post('/users', (req, res) => {
-    const user = { name: req.body.name, password: req.body.password };
-    users.push(user);
-    res.status(201).send();
+app.post('/users', async (req, res) => {
+    try {
+        // const salt = await bcrypt.genSalt() // Default is cost processing is 10
+        // const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        // console.log(salt);
+        // console.log(hashedPassword);
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { name: req.body.name, password: hashedPassword };
+        users.push(user);
+        res.status(201).send();
+    } catch {
+        res.status(500).send();
+    }
+});
+
+app.post('/users/login', async (req, res) => {
+   const user = users.find(user => user.name = req.body.name);
+   if(user == null) return res.status(400).send('Cannot Find User');
+   try {
+        if(await bcrypt.compare(req.body.password, user.password)){
+            res.send('Success');
+        } else {
+            res.send('Not Allowed');
+        }
+   } catch {
+        res.status(500).send();
+   }
 });
 
 app.listen(5000);
